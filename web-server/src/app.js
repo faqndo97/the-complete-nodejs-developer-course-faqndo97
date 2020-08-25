6eccb150307664a83c0efa64b6fad995978069cd
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-08-25 16:03:32 -0300

    Add hbs views and set view engine config to use hbs package

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,12 +4,35 @@ const path = require('path')
 // express is mainly a function, and we need start it
 const app = express()
 
+// With app.set we can overwrite default configurations by express.
+// In this case we are telling to express which module use to render
+// views.
+app.set('view engine', 'hbs')
 
 // Load static files
 const staticFolder = path.join(__dirname, '../public')
 app.use(express.static(staticFolder))
 
 // In this way we define routes
+app.get('', (req, res, next) => {
+  res.render('index', {
+    title: 'Weather app',
+    name: 'Facundo Espinosa'
+  })
+})
+
+app.get('/about', (req, res, next) => {
+  res.render('about', {
+    title: 'About'
+  })
+})
+
+app.get('/help', (req, res, next) => {
+  res.render('help', {
+    title: 'Help title'
+  })
+})
+
 app.get('/weather', (req, res, next) => {
   res.send({
     forecast: 'It is snowing',
