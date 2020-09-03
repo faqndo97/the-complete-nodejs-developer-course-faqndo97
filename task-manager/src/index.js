de426f051b15b31a561516f7892e24d20ff9691f
facundo espinosa <facundo.espinosa@fing.edu.uy>
2020-09-03 01:12:45 -0300

    add basic api to list and create Users and Task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,14 +1,55 @@
 const express = require('express')
 
+require('./db/mongoose')
+
+const User = require('./db/models/user')
+const Task = require('./db/models/task')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post('/', (req, res, next) => {
-  console.log(req.body)
-  res.send('It is alive')
+// Users
+
+app.get('/users', (req, res, next) => {
+  User.find().then((response) => {
+    res.status(201).send(response)
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
 })
+
+app.post('/users', (req, res, next) => {
+  const user = new User(req.body)
+
+  user.save().then((response) => {
+    res.status(201).send(response)
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
+})
+
+// Tasks
+
+app.get('/tasks', (req, res, next) => {
+  Task.find().then((response) => {
+    res.status(201).send(response)
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
+})
+
+app.post('/tasks', (req, res, next) => {
+  const task = new Task(req.body)
+
+  task.save().then((response) => {
+    res.status(201).send(response)
+  }).catch((error) => {
+    res.status(400).send(error)
+  })
+})
+
 
 app.listen(port, () => {
   console.log('Server is up!')
